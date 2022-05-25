183fe201dc53b0bfbd6d8719a81f30f1ef10fcaa
3ala2_Fadel <alaafadel1010@gmail.com>
2022-05-25 21:11:03 +0200

    database creation

---
+++ Model/data.py
@@ -0,0 +1,38 @@
+import sqlite3
+import os
+
+##create database called database
+conn = sqlite3.connect("database.db")
+
+# create a cursor
+c = conn.cursor()
+
+# create table , between """ """ write command
+#datatypes in sqllite :NULL - INTEGER - REAL(dicemal) - TEXT - BLOB(image-video)
+
+#c.execute("""CREATE TABLE accounts (
+#        user_name text,
+#        password text
+#
+#        )""")
+#
+
+#insert one record
+#c.execute("INSERT INTO accounts VALUES( 'Alaa','12345678')")
+#------------------------------------------------
+#c.execute("SELECT * FROM accounts")
+#print(c.fetchall())
+##print(c.fetchone()) #fetch the last one
+
+
+
+#% means start with Alaa then end by any thing
+c.execute("SELECT * FROM accounts ")
+print(c.fetchall())
+
+
+##to commit the excute by (push to database)
+conn.commit()
+
+#then close the connection
+conn.close()
